4b81d37bc984c1d3da996c322e540141806c1080
jeremylai7 <1049024592@qq.com>
2021-06-25 18:10:23 +0800

    添加消费测试

---
+++ spring-cloud-consumer-hystrix/src/main/java/com/neo/remote/HelloRemote.java
@@ -0,0 +1,17 @@
+package com.neo.remote;
+
+//import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by summer on 2017/5/11.
+ */
+@FeignClient(name= "service-provider", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+
+}
