7bae5c5457cb41f0950d90d40961b0e6f4118083
jeremylai7 <1049024592@qq.com>
2022-10-13 09:56:42 +0800

    修改seata调用

---
+++ nacos-consume-seata/src/main/java/com/seata/service/SeataService.java
@@ -0,0 +1,30 @@
+package com.seata.service;
+
+import io.seata.spring.annotation.GlobalTransactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author: laizc
+ * @date: created in 2022-10-13
+ * @desc:
+ */
+@Service
+public class SeataService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Transactional(rollbackFor = Exception.class)
+    @GlobalTransactional
+    public void placeOrder(Integer num) {
+        String result = restTemplate.getForObject("http://nacos-provide-order/order",String.class);
+        String result2 = restTemplate.getForObject("http://nacos-provide-stock/stock?num="+num,String.class);
+        System.out.println(result);
+    }
+
+}
+
+
