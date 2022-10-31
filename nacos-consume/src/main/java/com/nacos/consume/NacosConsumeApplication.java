358ec885fb7274500e6145958e866e0085a6f759
jeremylai7 <1049024592@qq.com>
2022-10-31 17:50:42 +0800

    更新nacos-consume配置

--- nacos-consume/src/main/java/com/nacos/consume/NacosConsumeApplication.java
+++ nacos-consume/src/main/java/com/nacos/consume/NacosConsumeApplication.java
@@ -3,22 +3,19 @@ package com.nacos.consume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+//@EnableFeignClients
 public class NacosConsumeApplication {
 
     public static void main(String[] args) {
-        new SpringApplication(NacosConsumeApplication.class).run(args);
-        Set<String> set =new HashSet<>();
-        set.add("aa");
-
-        //SpringApplication.run(NacosConsumeApplication.class, args);
+        //new SpringApplication(NacosConsumeApplication.class).run(args);
+        SpringApplication.run(NacosConsumeApplication.class, args);
     }
 
 }
