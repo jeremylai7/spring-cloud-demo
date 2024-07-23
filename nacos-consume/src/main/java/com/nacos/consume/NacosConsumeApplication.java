bc42aaa25e95dea1a0ce5354fc1c1a7f7e8c8424
laizc <1049024592@qq.com>
2024-07-23 11:01:19 +0800

    update config

--- nacos-consume/src/main/java/com/nacos/consume/NacosConsumeApplication.java
+++ nacos-consume/src/main/java/com/nacos/consume/NacosConsumeApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,11 +13,16 @@ import java.util.Set;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@RestController
 public class NacosConsumeApplication {
 
     public static void main(String[] args) {
-        //new SpringApplication(NacosConsumeApplication.class).run(args);
         SpringApplication.run(NacosConsumeApplication.class, args);
+    }
+
+    @GetMapping
+    public String index() {
+        return "this is consume-service";
     }
 
 }
--- nacos-provider/src/main/java/com/nacos/provider/NacosProviderApplication.java
+++ nacos-provider/src/main/java/com/nacos/provider/NacosProviderApplication.java
@@ -3,13 +3,21 @@ package com.nacos.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class NacosProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+    @GetMapping
+    public String index() {
+        return "this is provide-service";
     }
 
 }
