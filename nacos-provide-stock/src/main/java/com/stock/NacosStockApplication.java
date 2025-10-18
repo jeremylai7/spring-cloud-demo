767b1c305246204ce3992f6c237fd4d17b7e67f4
laizc <1049024592@qq.com>
2025-10-18 15:45:17 +0800

    add web依赖，修改配置占位

--- nacos-provide-stock/src/main/java/com/stock/NacosStockApplication.java
+++ nacos-provide-stock/src/main/java/com/stock/NacosStockApplication.java
@@ -3,8 +3,10 @@ package com.stock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.stock.dao")
 public class NacosStockApplication {
 
